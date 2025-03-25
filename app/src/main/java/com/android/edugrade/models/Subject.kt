c2ac4338e4f4d413a8ad031028a67349e1cf3137
jewican <james.ewican@cit.edu>
2025-03-25 18:15:44 +0800

    added timeslots to subject

--- app/src/main/java/com/android/edugrade/models/Subject.kt
+++ app/src/main/java/com/android/edugrade/models/Subject.kt
@@ -1,15 +1,24 @@
 package com.android.edugrade.models
 
+import java.time.LocalTime
+
 data class Subject(
-    val code: String,
+    val code: String = "-",
     val description: String = "",
+    val instructor: String = "",
     val units: Int = 0,
-    val assessmentTypes: List<AssessmentType>,
+    val assessmentTypes: List<AssessmentType> = listOf(),
+    val timeslots: List<Timeslot> = listOf(),
     val overallGrade: Double = 0.0
 )
 
+data class Timeslot(
+    val startTime: LocalTime,
+    val endTime: LocalTime
+)
+
 data class AssessmentType(
-    val name: String,
-    val grade: Double,
-    val weight: Double
+    val name: String = "",
+    val grade: Double = 0.0,
+    val weight: Double = 0.0
 )
