beff4a82a5ec66ca396a37926d51964aebeda833
l11x13w21 <292.v3n@gmail.com>
2025-03-26 12:44:01 +0800

    Final Layout + no fun

---
+++ app/src/main/java/com/android/edugrade/activities/DevPageActivity.kt
@@ -0,0 +1,14 @@
+package com.android.edugrade.activities
+
+import android.app.Activity
+import android.os.Bundle
+import com.android.edugrade.R
+
+class DevPageActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dev_page)
+
+
+    }
+}
