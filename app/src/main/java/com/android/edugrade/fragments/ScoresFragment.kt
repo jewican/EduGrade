75249ddde51e91197b9114359191f030f2dac806
jewican <james.ewican@cit.edu>
2025-03-27 10:11:32 +0800

    initial scoresfragment

--- app/src/main/java/com/android/edugrade/fragments/ScoresFragment.kt
+++ app/src/main/java/com/android/edugrade/fragments/ScoresFragment.kt
@@ -6,12 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.edugrade.R
+import com.android.edugrade.databinding.FragmentScoresBinding
+import com.android.edugrade.util.setCurrentFragment
+import dagger.hilt.android.scopes.FragmentScoped
 
 class ScoresFragment : Fragment(R.layout.fragment_scores) {
+    private lateinit var binding: FragmentScoresBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_scores, container, false)
+        binding = FragmentScoresBinding.inflate(inflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.addScoreButton.setOnClickListener {
+            setCurrentFragment(AddScoreFragment())
+        }
+    }
+
 }
