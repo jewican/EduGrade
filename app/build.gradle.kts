c2503287bc4a6d83ca72a5f77b95bcd97daa310f
jewican <james.ewican@cit.edu>
2025-03-28 00:39:34 +0800

    firebase dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+    implementation("com.google.firebase:firebase-auth")
+
 }
 
 kapt {
