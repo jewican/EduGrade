d219d134e209ad2d3a014a753739ed59fc2b9cc6
jewican <james.ewican@cit.edu>
2025-03-29 22:05:45 +0800

    initial score storage implementation

--- app/src/main/java/com/android/edugrade/data/score/ScoreStorage.kt
+++ app/src/main/java/com/android/edugrade/data/score/ScoreStorage.kt
@@ -1,11 +1,85 @@
 package com.android.edugrade.data.score
 
+import android.util.Log
+import com.android.edugrade.models.Score
 import com.android.edugrade.models.Subject
+import com.android.edugrade.util.toMap
+import com.android.edugrade.util.toScore
+import com.android.edugrade.util.toSubject
 import com.fatboyindustrial.gsonjavatime.Converters
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
 import com.google.gson.GsonBuilder
 
 class ScoreStorage {
-    private lateinit var scores: MutableList<Subject>
-    private val gson = Converters.registerLocalTime(GsonBuilder()).create()
-    private val filename = "subjects.json"
+    private var scores: MutableList<Score> = mutableListOf()
+    private val database = Firebase.database.reference
+    private val auth = Firebase.auth
+
+    private val TAG = "ScoreStorage"
+
+    fun addScore(
+        score: Score,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit) {
+        val userId = auth.currentUser?.uid
+        if (userId == null) {
+            Log.wtf(TAG, "User is not authenticated!")
+            return
+        }
+
+        Log.wtf(TAG, "Attempting to save ${score.name}...")
+
+        val scoreRef = database.child("scores")
+            .child(userId)
+            .child(score.code)
+            .push()
+
+        scoreRef.setValue(score.toMap())
+            .addOnSuccessListener {
+                Log.e(TAG, "Score saved successfully")
+                onSuccess()
+            }
+            .addOnFailureListener { e ->
+                Log.e(TAG, "Score saving error! ${e.message}")
+                onFailure(e)
+            }
+    }
+
+    fun loadScores() {
+        val userId = auth.currentUser?.uid
+        if (userId == null) {
+            Log.e(TAG, "User is not authenticated!")
+            return
+        }
+
+        val scoresRef = database
+            .child("scores")
+            .child(userId)
+
+        scoresRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                scores = mutableListOf()
+                for (subjectSnapshot in snapshot.children) {
+                    try {
+                        val map = subjectSnapshot.value as Map<String, Any>
+                        val score = map.toScore()
+                        scores.add(score)
+                    } catch (e: Exception) {
+                        Log.e(TAG, "Error parsing subject: ${e.message}")
+                    }
+                }
+
+                Log.e(TAG, "Retrieved subjects: $scores")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG, "Database error: ${error.message}")
+            }
+        })
+    }
 }
