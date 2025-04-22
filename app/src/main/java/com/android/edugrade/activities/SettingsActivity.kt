3928f285c12d087f439fab83281e39a50166ce41
Admin <bryan.tacoloy12@gmail.com>
2025-04-22 23:58:18 +0800

    Notif part still learning

--- app/src/main/java/com/android/edugrade/activities/SettingsActivity.kt
+++ app/src/main/java/com/android/edugrade/activities/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.android.edugrade.activities
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Switch
 import com.android.edugrade.R
 
 class SettingsActivity : Activity() {
@@ -9,5 +11,25 @@ class SettingsActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        val swNotif = findViewById<Switch>(R.id.notif)
+
+        swNotif.setOnClickListener{
+
+            //https://www.youtube.com/watch?v=hTdEPnsawiM&list=PLSrm9z4zp4mFttjku-3wiRkPH1lDRQLYy&index=2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
     }
 }
