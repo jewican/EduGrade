53814509c3b61695130a4eaece424ac3554829ec
jewican <james.ewican@cit.edu>
2025-05-06 02:14:32 +0800

    removed unused files

--- app/src/main/java/com/android/edugrade/models/BarChartData.kt
+++
@@ -1,7 +0,0 @@
-package com.android.edugrade.models
-
-data class BarChartData(
-    val subjectS: List<Subject> = listOf(),
-    val grades: List<Float>,
-    val title: String
-)
--- app/src/main/java/com/android/edugrade/models/LineChartData.kt
+++
@@ -1,7 +0,0 @@
-package com.android.edugrade.models
-
-data class LineChartData(
-    val assessments: List<Pair<String, Float>>,
-    val subjectName: String
-)
-
